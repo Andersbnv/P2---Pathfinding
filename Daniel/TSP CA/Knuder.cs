3c7560eccd0e80465f9eafb582f2f975706804a7
Daniel Mortensen <Daniel Mortensen@DESKTOP-FI2MI3R>
2019-03-26 14:21:07 +0100

    TSP CA FØRSTE COMMIT

---
+++ Daniel/TSP CA/Knuder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP_CA
+{
+    public class Knuder
+    {
+        public string Adress { get; set; }
+
+        public Knuder(string Adress)
+        {
+            this.Adress = Adress;
+        }
+
+        
+    }
+}
---
+++ Daniel/TSP CA/Relation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP_CA
+{
+    class Relation
+    {
+        public List<Tuple<string, int, string>> RelatiionList = new List<Tuple<string, int, string>>();
+
+        public void GetRelation(string adress1, int weight, string adress2)
+        {
+          RelatiionList.Add(Tuple.Create(adress1, weight, adress2));
+        }
+
+    }
+}
