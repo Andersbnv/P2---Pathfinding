542274cabaac639ea175098d11eac079d8de6c21
Anders Aaen Springborg <aaspringborg@gmail.com>
2019-05-01 22:24:22 +0200

    Still under tests...

--- Hungarian/Hungarian/Hungarian.cs
+++ Hungarian/Hungarian/Hungarian.cs
@@ -18,122 +18,169 @@ namespace Hungarian
             return locations;
         }
 
-        public void ReduceMatrix(double[,] Matrix)
+        public void FindMinAndReduceMatrix(double[,] matix)
         {
-            int rowLength = Matrix.GetLength(0);
-            int columnLength = Matrix.GetLength(1);
+            int rowLength = matix.GetLength(0);
+            int columnLength = matix.GetLength(1);
 
-            for (int i = 0; i < rowLength; i++)
+            for (int row = 0; row < rowLength; row++)
             {
                 double min = double.PositiveInfinity;
-                for (int j = 0; j < rowLength; j++)
+                for (int column = 0; column < rowLength; column++)
                 {
-                    if (Matrix[j, i] < min && Matrix[j, i] != inf)
+                    if (matix[column, row] < min && matix[column, row] != inf)
                     {
-                        min = Matrix[j, i];
+                        min = matix[column, row];
                     }
                 }
 
-                for (int k = 0; k < rowLength; k++)
+                for (int column = 0; column < rowLength; column++)
                 {
-                    if (Matrix[k, i] != inf)
+                    if (matix[column, row] != inf && min != inf)
                     {
-                        Matrix[k, i] -= min;
+                        matix[column, row] -= min;
                     }
                 }
             }
 
-            for (int i = 0; i < columnLength; i++)
+            for (int column = 0; column < columnLength; column++)
             {
                 double min = double.PositiveInfinity;
-                for (int j = 0; j < columnLength; j++)
+                for (int row = 0; row < columnLength; row++)
                 {
-                    if (Matrix[i, j] < min && Matrix[i, j] != inf)
+                    if (matix[column, row] < min && matix[column, row] != inf)
                     {
-                        min = Matrix[i, j];
+                        min = matix[column, row];
                     }
                 }
 
-                for (int k = 0; k < columnLength; k++)
+                for (int row = 0; row < columnLength; row++)
                 {
-                    if (Matrix[i, k] != inf)
+                    if (matix[column, row] != inf && min != inf)
                     {
-                        Matrix[i, k] -= min;
+                        matix[column, row] -= min;
                     }
                 }
             }
         }
         public double[,] AssignCost(double[,] Matrix)
         {
-            int rowLength = Matrix.GetLength(0);
-            int columnLength = Matrix.GetLength(1);
-            double[,] cost = new double[rowLength, columnLength];
+            int NumberOfColumns = Matrix.GetLength(0);
+            int NumberOfRows = Matrix.GetLength(1);
+            double[,] cost = new double[NumberOfColumns, NumberOfRows];
 
-            for (int i = 0; i < columnLength; i++)
+            for (int column = 0; column < NumberOfColumns; column++)
             {
-                for (int j = 0; j < rowLength; j++)
+                for (int row = 0; row < NumberOfRows; row++)
                 {
-                    double minRow = double.PositiveInfinity;
-                    double minColumn = double.PositiveInfinity;
-
-                    if (Matrix[j, i] == 0)
+                    cost[column, row] = double.NegativeInfinity;
+                }
+            }
+            
+            for (int row = 0; row < NumberOfRows; row++)
+            {
+                double minRow = double.PositiveInfinity;
+                double minColumn = double.PositiveInfinity;
+               
+                for (int column = 0; column < NumberOfColumns; column++)
+                {
+                    if (Matrix[column, row] == 0)
                     {
-                        int rowIndex = j;
-                        int columnIndex = i;
-                        for (int k = 0; k < rowLength; k++)
+                        int columnIndex = column; // c1
+                        int rowIndex = row;       // r0
+                        for (int columnCost = 0; columnCost < NumberOfColumns; columnCost++)
                         {
+                            if (columnIndex != columnCost && Matrix[columnCost, rowIndex] < minRow && Matrix[columnCost, rowIndex] != double.PositiveInfinity)
+                            {
+                                minRow = Matrix[columnCost, rowIndex]; // 0
+                            }
+                        }
 
-                            if (rowIndex != k && Matrix[k, columnIndex] < minRow)
-                            {
-                                minRow = Matrix[k, columnIndex];
-                            }
-                        }
-                        for (int k = 0; k < columnLength; k++)
+                        for (int rowCost = 0; rowCost < NumberOfRows; rowCost++)
                         {
-                            if (columnIndex != k && Matrix[rowIndex, k] < minColumn)
+                            if (rowIndex != rowCost && Matrix[columnIndex, rowCost] < minColumn && Matrix[columnIndex, rowCost] != double.PositiveInfinity)
                             {
-                                minColumn = Matrix[rowIndex, k];
+                                minColumn = Matrix[columnIndex, rowCost]; // 6
                             }
                         }
-                        cost[rowIndex, columnIndex] = minRow + minColumn;
+                        /*
+                        if (minRow == Double.PositiveInfinity)
+                        {
+                            minRow = 0;
+                        }
+
+                        if (minColumn == Double.PositiveInfinity)
+                        {
+                            minColumn = 0;
+                        }
+                        
+                        if (minRow == 0 && minColumn == 0)
+                        {
+                            cost[columnIndex, rowIndex] = Double.NegativeInfinity;
+                        }
+                        */
+                        cost[columnIndex, rowIndex] = minRow + minColumn; // [c1,r0] = 0 + 6                        
                     }
                 }
             }
+
+            
             return cost;
+        }
+
+        private void SpecialCase(double[,] cost)
+        {
+            var zerosList = new List<(int, int)>();
+            for (int column = 0; column < cost.GetLength(0); column++)
+            {
+                for (int row = 0; row < cost.GetLength(1); row++)
+                {
+                    if (cost[column, row] == 0)
+                    {
+                        zerosList.Add((column, row));
+                    }
+                }
+            }
+
+
+
         }
         public double[] FindRoute(double[,] Matrix, double[,] cost)
         {
             double[] outputArray = new double[2];
-            int rowLength = Matrix.GetLength(0);
-            int columnLength = Matrix.GetLength(1);
+            int NumberOfColumns = Matrix.GetLength(0);
+            int NumberOfRows = Matrix.GetLength(1);
             double maxCost = double.NegativeInfinity;
-            int maxIndexRow = 0;
-            int maxIndexColumn = 0;
+            int maxCostRowIndex = 0;
+            int maxCostColumnIndex = 0;
 
-            for (int i = 0; i < rowLength; i++)
+            for (int row = 0; row < NumberOfColumns; row++)
             {
-                for (int j = 0; j < columnLength; j++)
+                for (int column = 0; column < NumberOfRows; column++)
                 {
-                    if (cost[i, j] > maxCost)
+                    if (cost[row, column] > maxCost)
                     {
-                        maxIndexRow = i;
-                        maxIndexColumn = j;
-                        maxCost = cost[i, j];
+                        maxCostRowIndex = row;
+                        maxCostColumnIndex = column;
+                        maxCost = cost[maxCostRowIndex, maxCostColumnIndex];
                     }
                 }
             }
-            for (int i = 0; i < columnLength; i++)
+            for (int column = 0; column < NumberOfRows; column++)
             {
-                Matrix[i, maxIndexColumn] = inf;
+                Matrix[maxCostRowIndex, column] = inf;
             }
 
-            for (int j = 0; j < rowLength; j++)
+            for (int row = 0; row < NumberOfColumns; row++)
             {
-                Matrix[maxIndexRow, j] = inf;
+                Matrix[row, maxCostColumnIndex] = inf;
             }
-            Matrix[maxIndexColumn, maxIndexRow] = inf;
-            outputArray[0] = maxIndexRow;
-            outputArray[1] = maxIndexColumn;
+            Matrix[maxCostColumnIndex, maxCostRowIndex] = inf;
+
+            outputArray[0] = maxCostRowIndex;
+            outputArray[1] = maxCostColumnIndex;
+
+            Console.WriteLine("DISSE HØRER FUCKIGN SAMMEN"+ " " + outputArray[0]+ " " + outputArray[1]);
             return outputArray;
         }
         public double[] FindRoute2(double[,] Matrix, double[,] cost)
@@ -185,8 +232,8 @@ namespace Hungarian
                 Matrix[maxIndexRow, j] = inf;
             }
             Matrix[maxIndexColumn, maxIndexRow] = inf;
-            outputArray[1] = maxIndexRow;
-            outputArray[0] = maxIndexColumn;
+            Console.WriteLine(outputArray[1] = maxIndexRow);
+            Console.WriteLine(outputArray[0] = maxIndexColumn);
             return outputArray;
         }
 
@@ -213,17 +260,91 @@ namespace Hungarian
 
         public List<Vertex> Algorithm(Vertex startingPoint, List<Vertex> unsortedRoute)
         {
-            var Matrix = ListToMatrix(unsortedRoute);
+
+            //var Matrix = ListToMatrix(unsortedRoute);
+            
+            double inf = double.PositiveInfinity;
+            /*var Matrix = new double[9, 9]
+            {
+                { inf, 52, 44, 17, 81, 22, 3, 74, 21}, 
+                { 52, inf, 43, 70, 28, 44, 4, 16,  37}, 
+                { 44, 43, inf, 12, 50, 55, 5, 92, 68}, 
+                { 17, 70, 12, inf, 69, 66, 6, 50, 36 }, 
+                { 81, 28, 50, 69, inf, 77, 7, 43, 63},
+                { 22, 44, 55, 66, 77, inf, 8, 2, 30},
+                { 3,   4,  5,  6,  7,  8,  inf, 63, 71},
+                { 74, 16, 92, 50, 43, 2, 63,   inf, 55},
+                { 21, 37, 68, 36, 63, 30, 71,  55, inf }
+                
+
+            };
+            */
+             var Matrix = new double[10, 10]
+            {
+                { inf, 52, 44, 17, 81, 22, 3, 74, 21, 75}, 
+                { 52, inf, 43, 70, 28, 44, 4, 16,  37, 40}, 
+                { 44, 43, inf, 12, 50, 55, 5, 92, 68, 27}, 
+                { 17, 70, 12, inf, 69, 66, 6, 50, 36, 87 }, 
+                { 81, 28, 50, 69, inf, 77, 7, 43, 63, 78 },
+                { 22, 44, 55, 66, 77, inf, 8, 2, 30, 95},
+                { 3,   4,  5,  6,  7,  8,  inf, 63, 71, 67},
+                { 74, 16, 92, 50, 43, 2, 63,   inf, 55, 3 },
+                { 21, 37, 68, 36, 63, 30, 71,  55, inf, 23 },
+                { 75, 40, 27, 87, 78, 95, 67,  3, 23, inf },
+                
+            };
+             
+            /* {
+                { inf, 52, 44, 17, 81, 22, 3, 74, 50}, 
+                { 52, inf, 43, 70, 28, 44, 4, 16, 60 }, 
+                { 44, 43, inf, 12, 50, 55, 5, 92, 70}, 
+                { 17, 70, 12, inf, 69, 66, 6, 50, 80 }, 
+                { 81, 28, 50, 69, inf, 77, 7, 43, 90 },
+                { 22, 44, 55, 66, 77, inf, 8, 2,  33},
+                { 2,   4,  5,  6,  7,  8,  inf, 63, 12},
+                { 74, 16, 92, 50, 43, 2, 63,   inf, 37 },
+                { 50, 60, 70, 80, 90, 33, 12,   37, inf}
+                
+            };*/
+            /*
+             * {
+                { inf, 52, 44, 17, 81, 22, 3, 74, 50, 5 }, 
+                { 52, inf, 43, 70, 28, 44, 4, 16, 60, 10 }, 
+                { 44, 43, inf, 12, 50, 55, 5, 92, 70, 13 }, 
+                { 17, 70, 12, inf, 69, 66, 6, 50, 80, 47 }, 
+                { 81, 28, 50, 69, inf, 77, 7, 43, 90, 85 },
+                { 22, 44, 55, 66, 77, inf, 8, 2,  33, 83 },
+                { 2,   4,  5,  6,  7,  8,  inf, 63, 12, 38},
+                { 74, 16, 92, 50, 43, 2, 63,   inf, 37, 17 },
+                { 50, 60, 70, 80, 90, 33, 12,   37, inf, 71 },
+                { 5, 10, 13, 47, 85, 83, 38,   17, 71, inf }
+            };
+             */
+            /*double[,] Matrix = new double[12, 12] {
+                { inf, 52, 44, 17, 81, 11, 22, 44, 55, 77, 88, 99 },
+                { 52, inf, 43, 70, 28, 22, 44, 67, 87, 78, 34, 23 },
+                { 44, 43, inf, 12, 50, 33, 34, 32, 76, 34, 70, 50 },
+                { 17, 70, 12, inf, 69, 44, 34, 45, 56, 34, 65, 32 },
+                { 81, 28, 50, 69, inf, 55, 43, 65, 76, 34, 43, 43 },
+                { 11, 22, 33, 44, 55, inf, 43, 43, 43, 56, 43, 89 },
+                { 22, 44, 34, 34, 43, 43, inf, 43, 43, 56, 43, 89 },
+                { 44, 77, 32, 45, 65, 43, 43, inf, 43, 56, 43, 89 },
+                { 55, 87, 75, 56, 76, 43, 43, 43, inf, 56, 43, 89 },
+                { 77, 78, 34, 34, 34, 56, 56, 56, 56, inf, 43, 89 },
+                { 88, 34, 70, 65, 43, 43, 43, 43, 43, 43, inf, 89 },
+                { 99, 23, 50, 32, 43, 89, 89, 89, 89, 89, 89, inf }};*/
+
             string[] locations = RouteLocations(unsortedRoute);
             int matrixLength = Matrix.GetLength(0);
             int count = 0;
             while (count < matrixLength)
             {
-                ReduceMatrix(Matrix);
+                FindMinAndReduceMatrix(Matrix);
                 var cost = AssignCost(Matrix);
                 var route = FindRoute(Matrix, cost);
-                FindVertex(route, unsortedRoute, locations);
+                //FindVertex(route, unsortedRoute, locations);
                 count++;
+
             }
             return sortedRoute;
         }
@@ -237,11 +358,11 @@ namespace Hungarian
                 {
                     if (j == i)
                     {
-                        outputMatrix[i, j] = Double.PositiveInfinity;
+                        outputMatrix[j, i] = Double.PositiveInfinity;
                     }
                     else
                     {
-                        outputMatrix[i, j] = inputList[i].ConnectionList[j].Weight;
+                        outputMatrix[j, i] = inputList[i].ConnectionList[j].Weight;
                     }
                 }
             }
@@ -262,7 +383,6 @@ namespace Hungarian
                     double xDistance = route[i].XCoordinate - route[i - 1].XCoordinate;
                     double yDistance = route[i].YCoordinate - route[i - 1].YCoordinate;
                     distance += Math.Sqrt(xDistance * xDistance + yDistance * yDistance);
-                    Console.WriteLine(distance);
                 }
             }
 
--- Hungarian/Hungarian/Program.cs
+++ Hungarian/Hungarian/Program.cs
@@ -11,29 +11,18 @@ namespace Hungarian
     {
         static void Main(string[] args)
         {
-            var listInt = new List<int>() {
-                0,1,2,3,4,5,6,7,8,9,
-                10,11,12,13,14,15,16,17,18,19,
-                30,21,22,23,24,25,26,27,28,29
-                //20,31,32,33,34,35,36,37,38,39
-            };
-            var list = new List<Vertex>();
-            var tal = Enumerable.Range(1, 50).ToArray();
-            var random = new Random();
-            for (int i = 0; i < tal.Length; i++)
-            {
-                list.Add(new Vertex($"{tal[i]}", random.Next(150), random.Next(150)));
-            }
-            Console.WriteLine("Done");
-
+            string filePath = @"C:\Users\aaspr\Desktop\Test1_38.tsp";
+            Graph graph = new Graph(filePath);
             Stopwatch Timer = new Stopwatch();
+
+            Hungarian H = new Hungarian();
             Timer.Start();
-            var route = BruteForce.Algorithm(listInt, listInt[0]);
+            var route = H.Algorithm(graph.VertexList.ElementAt(0), graph.VertexList);
             Timer.Stop();
 
             Console.WriteLine("Time taken = " + Timer.Elapsed);
 
-            Console.WriteLine(route.TotalWeight());
+            Console.WriteLine(H.DistanceBetweenVertices(route));
             Console.ReadKey();
         }
     }
